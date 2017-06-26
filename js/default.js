3ed0bf99605897364c4b734fee2ab62733e2a3b4
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-26 18:33:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,6 +6,15 @@ document.getElementById('form').onsubmit = function(){
 for(var i=0; i < word; i++){
   var ValueTable = document.createElement('li');
   ValueTable.textContent = i + 1;
+  
+  if(ValueTable.textContent % 3 ==0){
+    ValueTable.TextContent = 'Fizz' ;
+  }else if(ValueTable.textContent % 5 == 0){
+    ValueTable.textContent ='Buzz' ;
+  }else if(ValueTable.textContent % 15 ==0){
+    ValueTable.textContent = 'Fizz Buzz!!';
+  }
+  
   document.getElementById('list').appendChild(ValueTable);
 }
 
