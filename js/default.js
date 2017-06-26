c4187ee38a47869646b698a956f4893f659c27f5
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-26 18:25:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,13 +2,14 @@ var word;
 document.getElementById('form').onsubmit = function(){
   word=parseInt(document.getElementById('form').word.value);
   window.alert(document.getElementById('form').word.value);
-//  return false;
-};
-
-
-
-for(var i=0; i < 100; i++){
+  
+for(var i=0; i < word; i++){
   var ValueTable = document.createElement('li');
   ValueTable.textContent = i + 1;
   document.getElementById('list').appendChild(ValueTable);
 }
+
+//  return false;
+};
+
+
