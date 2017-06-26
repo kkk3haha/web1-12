83b6b452638a0ae9b39a644c58d223d3f7293459
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-26 17:54:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@ document.getElementById('form').onsubmit = function(){
   return false;
 };
 
-for(var i=0; i < 100 ; i++){
+for(var i=0; i < word; i++){
   var ValueTable = document.createElement('ValueTable');
   ValueTable.textContent = i;
   document.getElementById('TableList').appendChild('ValueTable');
