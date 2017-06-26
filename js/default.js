bc8bfddcf5a3ee14328044aa82f383b5669798ce
kkk3haha <lieutenantcommander.mystical@gmail.com>
2017-06-26 18:22:30 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 var word;
 document.getElementById('form').onsubmit = function(){
-  word=document.getElementById('form').word.value;
+  word=parseInt(document.getElementById('form').word.value);
   window.alert(document.getElementById('form').word.value);
   return false;
 };
